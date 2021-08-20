2ba8fbef12b4a67c91e9b0b5f653b013b59be883
Kirill <kirillkekovalmaty@gmail.com>
2021-08-20 16:13:04 +0600

    build

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,16 +2,16 @@
   <div class="header-wrapper" >
     <div @click="changeLang" class="header-language">{{ $i18n.locale }}</div>
     <div class="header-content">
-      <div v-bind:class="{ 'header-buttons-active': currentRouteName=='/portfolio/' }" @click="changeRoute('/')"
+      <div v-bind:class="{ 'header-buttons-active': currentRouteName=='/portfolio/' }" @click="changeRoute('/portfolio/')"
            class="header-buttons">{{$t('Главная')}}
       </div>
-      <div v-bind:class="{ 'header-buttons-active': currentRouteName=='/portfolio//about_me' }" @click="changeRoute('about_me')"
+      <div v-bind:class="{ 'header-buttons-active': currentRouteName=='/portfolio/about_me' }" @click="changeRoute('/portfolio/about_me')"
            class="header-buttons">{{$t('Обо_мне')}}
       </div>
-      <div v-bind:class="{ 'header-buttons-active': currentRouteName=='/portfolio//works' }" @click="changeRoute('works')"
+      <div v-bind:class="{ 'header-buttons-active': currentRouteName=='/portfolio/works' }" @click="changeRoute('/portfolio/works')"
            class="header-buttons">{{$t('Работы')}}
       </div>
-      <div v-bind:class="{ 'header-buttons-active': currentRouteName=='/portfolio/contacts' }" @click="changeRoute('contacts')"
+      <div v-bind:class="{ 'header-buttons-active': currentRouteName=='/portfolio/contacts' }" @click="changeRoute('/portfolio/contacts')"
            class="header-buttons">{{$t('Контакты')}}
       </div>
     </div>
