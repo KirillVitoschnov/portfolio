0aadc5b69eab4bc9463022fc6538024c323e5e59
Kirill <kirillkekovalmaty@gmail.com>
2021-08-20 16:02:08 +0600

    build

--- src/router/router.js
+++ src/router/router.js
@@ -10,7 +10,7 @@ let router= new Router ({
     mode: 'history',
     routes: [
         {
-            path: 'portfolio/',
+            path: '/portfolio/',
             component: mainPage
         },
         {
