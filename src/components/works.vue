36bf34d5b1501fe37ecf7ed42f225959d8dac438
kirill vitoschnov <kirillkekovalmaty@gmail.com>
2021-10-16 18:36:03 +0600

    works

--- src/components/works.vue
+++ src/components/works.vue
@@ -17,12 +17,17 @@
           </div>
           <span class="work-description-text">{{item.description}}</span>
           <div class="work-description-more" v-if="item.showMore">
-            {{item.title}}
-            <ul>
+            <div class="work-description-more-title">{{item.title}}</div>
+            <div @click="item.showMore=!item.showMore" class="close"/>
+            <div style="padding: 10px">{{item.description}}</div>
+            <ul style="margin-top: 20px">
               <li v-for="tech in item.technologys" :key="tech" >{{tech}}</li>
             </ul>
-            {{item.projectUrl}}
-            {{item.sourceUrl}}
+            <div class="url-wrapper">
+              <a target="_blank" :href="item.projectUrl" class="works-show"></a>
+              <a target="_blank" :href="item.sourceUrl" class="works-github"></a>
+            </div>
+
           </div>
         </div>
 
@@ -40,20 +45,22 @@ export default {
     return {
       works:[
         {
-          showMore:true,
+          showMore:false,
           title:'Игра змейка',
           description:'компьютерная игра, впервые появившаяся в кнопочном телефоне Nokia 6110. Разработана финским разработчиком Танели Арманто и выпущена компанией Nokia.',
           technologys:['html5','css','canvas','javascript'],
           image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkiT-yyLmJNm-LFIkIuz04Wd-sOUYRrpt-bQ&usqp=CAU',
           projectUrl:'https://www.google.com/',
           sourceUrl:'https://www.google.com/',
-        }
+        },
+
 
       ]
 
 
     }
   },
+
 }
 
 </script>
