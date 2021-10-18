39309e557aa640e2c05d7ea8cf915da7c740312c
kirill vitoschnov <kirillkekovalmaty@gmail.com>
2021-10-19 00:18:55 +0600

    works

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="content" >
 <div class="contact-desk">
+  <h2>Обратная связь</h2>
   <div class="contact-wrapper">
     <div class="contact-wrapper-item-hh"><div class="contact-item-text"><a target="_blank" href="https://hh.kz/resume/cf026cbcff090b0db40039ed1f6433386d4849">Мое резюме</a></div></div>
     <div class="contact-wrapper-item-wh"><div class="contact-item-text"><a target="_blank" href="https://wa.me/+77075734923">Whatsapp</a></div></div>
