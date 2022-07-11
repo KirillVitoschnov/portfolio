82886879ed0f7b38b6bce98886349dde924436d9
Kirill <kirill.vitoschnov@ts-technology.kz>
2022-07-11 23:19:12 +0600

    update info

---
+++ src/components/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content" >
+<div class="contact-desk">
+  <h2>{{$t('ОбратнаяСвязь.ОбратнаяСвязь')}}</h2>
+  <div class="contact-wrapper">
+    <div class="contact-wrapper-item-hh"><div class="contact-item-text"><a target="_blank" href="https://hh.kz/resume/cf026cbcff090b0db40039ed1f6433386d4849">{{$t('ОбратнаяСвязь.МоеРезюме')}}</a></div></div>
+    <div class="contact-wrapper-item-wh"><div class="contact-item-text"><a target="_blank" href="https://wa.me/+77075734923">Whatsapp</a></div></div>
+<!--    <div class="contact-wrapper-item-linin"><div class="contact-item-text">headhunter</div></div>-->
+    <div class="contact-wrapper-email"><div class="contact-item-text"><a href="mailto:kirill.vitoschnov@yandex.ru">kirill.vitoschnov@yandex.ru</a></div></div>
+    <div  class="contact-wrapper-github"><div class="contact-item-text"><a target="_blank" href="https://github.com/KirillVitoschnov">https://github.com/KirillVitoschnov</a></div></div>
+
+  </div>
+
+
+</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'contact',
+  data() {
+    return {}
+  }
+}
+</script>
